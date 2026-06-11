434ee3c1cadf35e7302b84bda671f0fae861d0df
AparicioJohan <johan.aparicio@correounivalle.edu.co>
2026-06-10 23:05:25 -0500

    feat new dataset 2022

--- R/00_dt_potato.R
+++ R/00_dt_potato.R
@@ -1,4 +1,4 @@
-#' Drone-derived data from a potato breeding trial
+#' Drone-derived data from a potato breeding trial (2020)
 #'
 #' Canopy and Green Leaf Index for a potato trial arranged in a p-rep design.
 #'
@@ -13,5 +13,24 @@
 #'   \item{Canopy}{dbl Canopy UAV-Derived}
 #'   \item{GLI}{dbl Green Leaf Index UAV-Derived}
 #' }
-#' @source UW - Potato Breeding Program
+#' @source UW - Potato Breeding Program 2020
 "dt_potato"
+
+#' Drone-derived data from a potato breeding trial (2022)
+#'
+#' Ground cover and plant height for a potato trial arranged in a p-rep design.
+#'
+#' @format A tibble with 1764 rows and 9 variables:
+#' \describe{
+#'   \item{Trial}{chr trial name}
+#'   \item{Plot}{dbl denoting the unique plot id}
+#'   \item{Row}{dbl denoting the row coordinate}
+#'   \item{Range}{dbl denoting range coordinate}
+#'   \item{gid}{chr denoting the genotype id}
+#'   \item{DAP}{dbl denoting Days after planting}
+#'   \item{GC}{dbl UAV-Derived Ground Cover}
+#'   \item{PH}{dbl UAV-Derived Plant Height}
+#'   \item{Yield}{Total Yield}
+#' }
+#' @source UW - Potato Breeding Program 2022
+"dt_potato_22"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,8 +7,11 @@ dt_potato <- readr::read_csv("data-raw/data_2020_flexfitr.csv") |>
 usethis::use_data(dt_potato, overwrite = TRUE)
 
 
-dt_potato_22 <- readr::read_csv("data-raw/chips_2022.csv") |>
-  select(Trial, DAP, Plot, row, range, Name, Total.yield, vine.maturity, Red:Canopy)
-names(dt_potato_22) <- names(dt_potato_20)
+# dt_potato_22 <- readr::read_csv("data-raw/chips_2022.csv") |>
+#   select(Trial, DAP, Plot, row, range, Name, Total.yield, vine.maturity, Red:Canopy)
+# names(dt_potato_22) <- names(dt_potato_20)
+
+dt_potato_22 <- readr::read_csv("data-raw/dt_potato_2022_paper.csv") |>
+  select(Trial, Plot, Row, Range, gid, DAP, GC, PH = ph, Yield = yield)
 
 usethis::use_data(dt_potato_22, overwrite = TRUE)
